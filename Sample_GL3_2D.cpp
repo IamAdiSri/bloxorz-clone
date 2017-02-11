f8e720dfb442e4e21ee82147476081d21f667980
Aditya Srivastava <aditya.srivastava@research.iiit.ac.in>
2017-02-11 19:33:46 +0530

    Changed view to display in 3D. Fixed cuboid.

--- Sample_GL3_2D.cpp
+++ Sample_GL3_2D.cpp
@@ -461,10 +461,10 @@ void createCuboid()
 	};
 
 	// create3DObject creates and returns a handle to a VAO that can be used later
-	cuboid = create3DObject(GL_TRIANGLES, 12, vertex_buffer_data, color_buffer_data, GL_FILL);
+	cuboid = create3DObject(GL_TRIANGLES, 12*3, vertex_buffer_data, color_buffer_data, GL_LINE);
 }
 
-float camera_rotation_angle = 90;
+float camera_rotation_angle = 120;
 float rectangle_rotation = 0;
 float triangle_rotation = 0;
 
@@ -480,16 +480,16 @@ void draw ()
 	glUseProgram (programID);
 
 	// Eye - Location of camera. Don't change unless you are sure!!
-	glm::vec3 eye ( 5*cos(camera_rotation_angle*M_PI/180.0f), 0, 5*sin(camera_rotation_angle*M_PI/180.0f) );
+	glm::vec3 eye ( 5*cos(camera_rotation_angle*M_PI/180.0f), 2, 5*sin(camera_rotation_angle*M_PI/180.0f) );
 	// Target - Where is the camera looking at.  Don't change unless you are sure!!
 	glm::vec3 target (0, 0, 0);
 	// Up - Up vector defines tilt of camera.  Don't change unless you are sure!!
 	glm::vec3 up (0, 1, 0);
 
 	// Compute Camera matrix (view)
-	// Matrices.view = glm::lookAt( eye, target, up ); // Rotating Camera for 3D
+	Matrices.view = glm::lookAt( eye, target, up ); // Rotating Camera for 3D
 	//  Don't change unless you are sure!!
-	Matrices.view = glm::lookAt(glm::vec3(0,0,3), glm::vec3(0,0,0), glm::vec3(0,1,0)); // Fixed camera for 2D (ortho) in XY plane
+	// Matrices.view = glm::lookAt(glm::vec3(0,0,3), glm::vec3(0,0,0), glm::vec3(0,1,0)); // Fixed camera for 2D (ortho) in XY plane
 
 	// Compute ViewProject matrix as view/camera might not be changed for this frame (basic scenario)
 	//  Don't change unless you are sure!!
